6bc93cdd59cbb08bacf593e9c6d3c24977317d7d
Aris Wibowo <aris.wibowo@elysiannxt.com>
2025-10-27 12:49:31 +0700

    update sql

--- 20251022/build-econnect/PublishOutput/Domains/Extraction/ExtractionTypes/Ayda/Extraction/Queries/Contracts.sql
+++ 20251022/build-econnect/PublishOutput/Domains/Extraction/ExtractionTypes/Ayda/Extraction/Queries/Contracts.sql
@@ -1,8 +1,10 @@
 ﻿SELECT
 	reporting_date
+	, branch_code
 	, origin_contract_id
 	, id_pihak_lawan
 	, nilai_ayda as outstanding_amount
+	, customer_name
 FROM
   ayda
 WHERE
--- 20251022/build-econnect/PublishOutput/Domains/Extraction/ExtractionTypes/Penyertaan/Extraction/Queries/Contracts.sql
+++ 20251022/build-econnect/PublishOutput/Domains/Extraction/ExtractionTypes/Penyertaan/Extraction/Queries/Contracts.sql
@@ -1,10 +1,12 @@
 ﻿SELECT
 	reporting_date
 	, reporting_date + INTERVAL '1 month' AS reporting_date_plus_1month
+	, branch_code
 	, origin_contract_id
 	, id_pihak_lawan
 	, nilai_tercatat as outstanding_amount
 	, hari_tunggakan
+	, customer_name
 FROM
   penyertaan
 WHERE
